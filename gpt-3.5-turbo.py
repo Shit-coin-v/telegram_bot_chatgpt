52103b8e8ee3a41d36206e6de5a1a2f48e13ec2e
Shit-coin-v <vasyakriv8688@gmail.com>
2023-05-11 08:26:41 +0300

    new memmory

--- gpt-3.5-turbo.py
+++ gpt-3.5-turbo.py
@@ -10,15 +10,23 @@ bot = telebot.TeleBot(TOKEN_FOR_BOT_GPT)
 def send_welcome(message):
     bot.send_message(message.chat.id, "Привет! Я чат бот можете спросить, что угодно")
 
+messeges = [
+    {"role": "system", "content": "Your job is to respond to customer feedback, in the marketplace wildberries. Our Product Women's Oversize Shirt."},
+    {"role": "user", "content": "The color does not match, much darker."},
+    {"role": "manager", "content": "I am sorry to hear that the color did not meet your expectations and was much darker. We will pass the information on to quality control"},
+    ]
+
+def update(messages, role, content):
+    messages.append({'role': role, 'content': content})
+    return messages
+
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
     try:
+        update(messeges, 'user', message.text)
         response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "you are the marketplace manager in charge of reviews. our product is a 'women's oversize shirt'. We work under the fbs system, so we do not have access to the product itself and the packaging of the product may not come in the best condition. You should not ask for phone number or contacts from the customer this is forbidden by the marketplace. If a bad review comes in just apologize, don't promise anything. Our store has no feedback system. We are only sellers in the marketplace. Responses are only in Russian."},
-            {"role": "user", "content": message.text},
-        ],
+        messages=messeges
 )
         bot.reply_to(message, response.choices[0]['message']['content'])
 
